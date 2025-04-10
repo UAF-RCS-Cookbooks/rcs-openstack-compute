db518354f84f6265c75d1f834dba72177d4db28a
Jeremiah Dabney <jdabney@gmail.com>
2025-04-09 22:10:11 -0800

    Cleared versions.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'UAF RCS'
 maintainer_email 'chef@rcs.alaska.edu'
 license          'Apache-2.0'
 description      'The OpenStack Compute service Nova.'
-version          '20.0.6'
+version          '20.0.7'
 
 chef_version '>= 16.0'
 
@@ -11,9 +11,9 @@ chef_version '>= 16.0'
   supports os
 end
 
-depends 'apache2', '~> 8.6'
-depends 'rcs-openstack-common', '>= 20.0.0'
-depends 'rcs-openstack-identity', '>= 20.0.0'
-depends 'rcs-openstack-image', '>= 20.0.0'
-depends 'rcs-openstack-network', '>= 20.0.0'
-depends 'rcs-openstackclient', '>= 20.0.0'
+depends 'apache2'
+depends 'rcs-openstack-common'
+depends 'rcs-openstack-identity'
+depends 'rcs-openstack-image'
+depends 'rcs-openstack-network'
+depends 'rcs-openstackclient'
